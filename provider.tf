4998b5ee4aa0943bfe2e86b4843cc76738c2461c
ans1403 <akrnose1403@gmail.com>
2022-04-27 01:24:14 +0900

    chore: backend設定をハードコーディングへ変更

--- provider.tf
+++ provider.tf
@@ -6,8 +6,11 @@ terraform {
     }
   }
 
-  # will be configurated by environment value
-  backend "s3" {}
+  backend "s3" {
+    region = "ap-northeast-1"
+    bucket = "tfstate"
+    key    = "terraform.tfstate"
+  }
 }
 
 # will be configurated by environment value
