e8036ee25bc18d3d8fca99d7774447b49a5ee149
ans1403 <akrnose1403@gmail.com>
2022-04-27 00:55:53 +0900

    chore: providerファイルの追加

---
+++ provider.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 4.11.0"
+    }
+  }
+
+  # will be configurated by environment value
+  backend "s3" {}
+}
+
+# will be configurated by environment value
+provider "aws" {}
