c2779333bfbcb89c2e40dbdb42305b45724dd438
ans1403 <akrnose1403@gmail.com>
2022-07-16 22:53:40 +0900

    chore: fargateのタスクは別で作る。

--- modules/ecs_cluster/main.tf
+++ modules/ecs_cluster/main.tf
@@ -1,5 +1,23 @@
+locals {
+  cluster_name = "my-ecs-cluster"
+  repository_names = [
+    "test1",
+    "test2",
+  ]
+}
+
 # ===== ECS =====
 
 resource "aws_ecs_cluster" "main" {
-  name = "my-ecs-cluster"
+  name = local.cluster_name
+}
+
+# ===== ECR =====
+
+resource "aws_ecr_repository" "main" {
+  name = [for name in local.repository_names : name]
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
 }
--- modules/ecs_cluster/output.tf
+++ modules/ecs_cluster/output.tf
@@ -1,3 +1,1 @@
-output "ecs_cluster_id" {
-  value = aws_ecs_cluster.main.id
-}
+
